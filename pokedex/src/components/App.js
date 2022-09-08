fe65d295d67dc3846fbaef485a63ed62adbd9642
Henrique <henrique.moriggi@hotmail.com>
2022-09-08 15:00:57 -0300

    Criando os arquivos

---
+++ pokedex/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Ola Mundo!
+      </div>
+    );
+  }
+}
+
+export default App;
