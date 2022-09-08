ae3c791480a97d7540a3f22212a30926fcc1e8ed
Henrique <henrique.moriggi@hotmail.com>
2022-09-08 15:09:09 -0300

    Resolvendo o erro de serviceWorker

--- pokedex/src/components/App.js
+++ pokedex/src/components/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import './styles/App.css';
-
+import './styles/App.css'
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
   render() {
     return (
-      <div className="App">
-        Ola Mundo!
+      <div className = "App">
+
       </div>
     );
   }
